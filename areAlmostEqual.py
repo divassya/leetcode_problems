6745e49b30985fede460f5e060fdac65b7f8f9b7
Assiya Karatay <61145269+divassya@users.noreply.github.com>
2022-10-18 18:57:45 -0400

    Create areAlmostEqual.py

---
+++ areAlmostEqual.py
@@ -0,0 +1,51 @@
+"""
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 100
+s1.length == s2.length
+s1 and s2 consist of only lowercase English letters.
+"""
+class Solution(object):
+    def areAlmostEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if s1==s2:
+            return True
+        count=0
+        for r in range(len(s1)):
+            if s1[r]!=s2[r]:
+                count+=1
+        
+        if count==2:
+            if sorted(s1)==sorted(s2):
+                return True
+            else:
+                return False
+        else:
+            return False
