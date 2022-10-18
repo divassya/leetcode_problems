714115df094eac1503c9134a56416a0edee67d92
Assiya Karatay <61145269+divassya@users.noreply.github.com>
2022-10-18 19:06:40 -0400

    Create isSubsequence.py

---
+++ isSubsequence.py
@@ -0,0 +1,42 @@
+'''
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+'''
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) > len(t):
+            return False
+        if len(s) == 0:
+            return True
+        subsequence=0
+        for i in range(len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s)
